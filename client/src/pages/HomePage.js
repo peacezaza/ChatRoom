b3feec969d028941e819b4ee2577386eccd91e64
kanisorn <qawqaw63123@gmail.com>
2024-02-29 15:40:17 +0700

    make home page a little bit bettter

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -61,7 +61,7 @@ function App(){
 
             <div className="white-text-box">
                 <h1 className='centerText'>IMAGINE PLACE...</h1>
-                <p>...where you can belong to a school club, a gaming group, or a worldwide art community.
+                <p className='centerText'>...where you can belong to a school club, a gaming group, or a worldwide art community.
                     Where just you and a handful of friends can spend time together. A place that makes it easy
                     to talk every day and hang out more often.</p>
             </div>
