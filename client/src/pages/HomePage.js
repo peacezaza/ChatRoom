7c28d6cce7e8db7a18dc524d5defa99482051e37
kanisorn <qawqaw63123@gmail.com>
2024-02-29 15:05:14 +0700

    front end add some detail on home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import SignIn from './SignIn'
 import { Outlet, Link } from "react-router-dom";
-
+import HomePage from './HomePage.css'
 
 function App(){
     const divStyle = {
@@ -23,12 +23,12 @@ function App(){
         marginTop: '25px',
     };
     const buttonStyle = {
-        backgroundColor: 'white',
-        border: 'none', 
-        color: 'black', 
+        backgroundColor: '#ffffff',
+        border: 'none',
+        color: 'black',
         fontSize: '20px',
         fontWeight: 'bold',
-        textAlign: 'center', 
+        textAlign: 'center',
         marginRight: '20px',
         borderRadius: '30px',
     };
@@ -44,21 +44,29 @@ function App(){
     };
     const handleCrickUp = (event) => {
         event.target.style.backgroundColor = 'white'; // When Crick
-        
     };
+
     return (
         <div style={divStyle}>
             <div style={{display: 'flex', justifyContent: 'space-between'}}>
                 <img src="write_circle.png" alt="Logo" width={65} height={65} style={{margin: 20}}/>
                 <img src="logos_braze-icon.png" alt="Logo" width={50} height={50} style={{position: 'absolute', top: 24, left: 27}}/>
                 <h1 style={textStyle}>About</h1>
-                
+
                 <div style={{marginTop: '4.5vh', justifyContent: 'space-between'}}>
-                    <Link  to = {'/signup'}><input type="submit" value={"Sign up"} style={buttonStyle} onMouseEnter={handleHover} onMouseLeave={handleLeave} onMouseDown={handleCrickDown} onMouseUp={handleCrickUp} /></Link>
-                    <Link  to = {'/signin'}><input type="submit" value={"Sign in"} style={buttonStyle} onMouseEnter={handleHover} onMouseLeave={handleLeave} onMouseDown={handleCrickDown} onMouseUp={handleCrickUp} /></Link>
+                    <Link to={'/signup'}><input type="submit" value={"Sign up"} style={buttonStyle} onMouseEnter={handleHover} onMouseLeave={handleLeave} onMouseDown={handleCrickDown} onMouseUp={handleCrickUp} /></Link>
+                    <Link to={'/signin'}><input type="submit" value={"Sign in"} style={buttonStyle} onMouseEnter={handleHover} onMouseLeave={handleLeave} onMouseDown={handleCrickDown} onMouseUp={handleCrickUp} /></Link>
                 </div>
+            </div>
+
+            <div className="white-text-box">
+                <h1 className='centerText'>IMAGINE PLACE...</h1>
+                <p>...where you can belong to a school club, a gaming group, or a worldwide art community.
+                    Where just you and a handful of friends can spend time together. A place that makes it easy
+                    to talk every day and hang out more often.</p>
             </div>
         </div>
     )
 }
+
 export default App;
