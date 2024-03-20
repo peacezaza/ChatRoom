8803fccae109e61dc5fabbbef9bda65cd9d6a333
kanisorn <qawqaw63123@gmail.com>
2024-03-20 22:45:50 +0700

    chat page

--- client/src/pages/private/ServerPage.js
+++ client/src/pages/private/ServerPage.js
@@ -7,12 +7,23 @@ const get_server = ()=>{
 function ServerPage(){
     return(
         <div className='con'>
-            <div className="side-bar"></div>
+            <div className="side-bar">
+                <div>
+                    <button className='addServer'></button>
+                </div>
+            </div>
             <div className="content">
                 <div className='nav-bar'></div>
                 <div className='channel'>
                     <div className='side-bar-channel'></div>
-                    <div className='chat'></div>
+                    <div className='chat'>
+                        <div>
+                            <input type='text' className='ChatInput' placeholder='TypeHere...'/>
+                        </div>
+                        <div>
+                            <button className='SubmitChat'></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
